b5dc6aee475a76f7a0e5f28b4377d2544b9564bf
SolightzZ <reimdes1@gmail.com>
2024-11-21 16:26:14 +0700

    Update Dart_io.dart

--- dart_application_1/bin/Dart_io.dart
+++ dart_application_1/bin/Dart_io.dart
@@ -1,6 +1,20 @@
 import 'dart:io';
 
+String findOddEven(int n) {
+  if (n % 2 == 0)
+    return "Even";
+  else
+    return "Odd";
+}
+
 void main() {
+  print(findOddEven(5));
+  print(findOddEven(8));
+  print(findOddEven(20));
+
+  sayhi("Paniti Jahem");
+
+/*
   print("Emter your name => ");
   String? name = stdin.readLineSync();
   print("Enter your age => ");
@@ -8,4 +22,8 @@ void main() {
 
   print("--------------------------------------------------");
   print("Name ; $name \nAge : $age");
+
+  */
 }
+
+void sayhi(var name) => print("HI .... $name");
